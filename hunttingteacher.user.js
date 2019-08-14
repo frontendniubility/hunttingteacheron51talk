978a8aade4aa60d3be8083a6eb6d87f313831118
68681395 <happycoder@foxmail.com>
2019-08-14 15:46:39 +0800

    hunttingteacher.user.js

--- hunttingteacher.user.js
+++ hunttingteacher.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         辅助选老师-有效经验值|好评率|年龄|Top 5
-// @version      0.1.7
+// @version      0.1.9
 // @namespace    https://github.com/niubilityfrontend
 // @description  51Talk.辅助选老师-有效经验值|好评率|年龄|Top 5；有效经验值=所有标签数量相加后除以5；好评率=好评数/总评论数；年龄根据你的喜好选择。
 // @author       jimbo
@@ -97,8 +97,12 @@
         + 'href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" '
         + 'rel="stylesheet" type="text/css">'
     );
-     $("head").append('<style type="text/css">.search-teachers .s-t-list .item-time-list {margin-top:315px;}'
-                     +' .search-teachers .s-t-list .item {   height: 679px; }</style>');
+    $("head").append('<style type="text/css">'
+                     +'.search-teachers .s-t-list .item-time-list {margin-top:315px;}'
+                     +' .search-teachers .s-t-list .item {   height: 679px; }'
+                     +'.search-teachers .s-t-list .s-t-content { margin-right: 0px;}'
+                     +'.search-teachers { width: 100%; }'
+                    +'</style>');
 
 
     function sleep(delay) {
@@ -338,6 +342,7 @@
     });
 
     submit(function (next) {
+        alert(1);
         $('.s-t-list').before($(".s-t-page").prop('outerHTML'));
         $('#filterdialog').dialog("open");
         $('#filterdialog').parent().scrollFix();
