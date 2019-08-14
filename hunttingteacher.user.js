d49553ab3c94c661745a48cfb4c9abe34cabd787
68681395 <happycoder@foxmail.com>
2019-08-14 16:43:47 +0800

    automate commit

--- hunttingteacher.user.js
+++ hunttingteacher.user.js
@@ -17,7 +17,7 @@
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // ==/UserScript==
-(function () {//test 1
+(function () {
     'use strict';
     ; (function ($) {
         jQuery.fn.scrollFix = function (height, dir) {
